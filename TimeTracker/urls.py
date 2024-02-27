a91001a9df34f124fdc7f13a1a5bd85fb4bddf09
RONG2024 <2840486C@student.gla.ac.uk>
2024-02-27 15:08:48 +0800

    added link

--- TimeTracker/urls.py
+++ TimeTracker/urls.py
@@ -18,11 +18,5 @@ urlpatterns = [
     # profile
     path('profile/<slug:username>/',views.profile, name='profile'),
     path('profile/update/<slug:username>', views.profile_update, name='profile_update'),
-    path('userInfo/<slug:username>/', views.index),
-    path('report/<slug:username>/', views.report),
-    path('Group/<slug:username>/', views.table),
-    path('MusicList/<slug:username>/', views.music),
-    path('Coin/<slug:username>/', views.coin),
-    path('Settings/<slug:username>/', views.setting),
-    path('Badges/<slug:username>/', views.badges),
+
 ]
