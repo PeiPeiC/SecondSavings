e609839820f3276701e217edd6c6b81dadd6516f
Lixingwan Cao <2750536c@student.gla.ac.uk>
2024-02-28 13:38:07 +0000

    support change avatar

--- TimeTracker/views.py
+++ TimeTracker/views.py
@@ -10,6 +10,8 @@ from django.core.files.base import ContentFile
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
