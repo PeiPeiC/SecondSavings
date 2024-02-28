2a798a46454abc5da4440e339d8eda81273f83c2
Lixingwan Cao <2750536c@student.gla.ac.uk>
2024-02-28 16:05:44 +0000

    update avatar update

--- TimeTracker/views.py
+++ TimeTracker/views.py
@@ -10,9 +10,14 @@ from django.core.files.base import ContentFile
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
+import base64
 import json
+from io import BytesIO
 
+from PIL import Image
 from django.contrib.auth.decorators import login_required
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
