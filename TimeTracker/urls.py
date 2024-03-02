3baad742ac33bd622a8d6f192b66fd1291262ff8
PeiPeiC <l86426@gmail.com>
2024-03-02 21:11:13 +0000

    rebase on DEVELOP and modify the urls.py in app and project root

--- TimeTracker/urls.py
+++ TimeTracker/urls.py
@@ -4,8 +4,16 @@ from TimeTracker import views
 app_name = 'TimeTracker'
 
 urlpatterns = [
+    path('', views.main, name='main'),
+    path("main/", views.main, name='main'),
+    path('report/', views.report, name='report'),
+    path('Group/', views.table, name='table'),
+    path('MusicList/', views.music, name='music'),
+    path('Coin/', views.coin, name='coin'),
+    path('Settings/', views.setting, name='setting'),
+    path('Badges/', views.badges, name='badges'),
     # profile
-    path('profile/',views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('profile/update/', views.profile_update, name='profile_update'),
     path('profile/avater_update/', views.avatar_update, name='avatar_update'),
 
@@ -13,8 +21,4 @@ urlpatterns = [
     path('setting/', views.setting, name='setting'),
     path('setting/sync_google_task', views.setting_sync, name='setting_sync_google_task'),
     path('setting/update_alarm', views.alarm_update, name='setting_update_alarm'),
-
-    path('', views.main, name='main'),
-    path("main/", views.main, name='main'),
-
 ]
