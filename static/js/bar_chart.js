26083de95b8b57e43efe805ff103e8e845a358ab
RONG2024 <2840486C@student.gla.ac.uk>
2024-03-14 01:33:05 +0800

    new report

--- static/js/bar_chart.js
+++ static/js/bar_chart.js
@@ -1,17 +1,56 @@
- var dom = document.getElementById('container');
+        var dom = document.getElementById('container');
         var myChart = echarts.init(dom, null, {
             renderer: 'canvas',
             useDirtyRect: false
         });
-        var app = {};
-        var option;
-        var weekData = [120, 132, 101, 134, 90, 230, 210];
-        var monthData = [220, 182, 191, 234, 290, 330, 310];
-        var yearData = [150, 232, 201, 154, 190, 330, 410];
 
-        var weekXAxisData = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-        var monthXAxisData = ['Week 1', 'Week 2', 'Week 3', 'Week 4'];
-        var yearXAxisData = ['2018', '2019', '2020', '2021', '2022', '2023', '2024'];
+        // var labels = dom.getAttribute('data-labels');
+        // var taskTotalTime = dom.getAttribute('data-task-total-time');
+        // var breakTotalTime = dom.getAttribute('data-break-total-time');
+        // var app = {};
+        var option;
+        // var weekData = ['120', '132', '101', '134', '90', '230', '210'];
+        // var monthData = [220, 182, 191, 234, 290, 330, 310];
+        // var yearData = [150, 232, 201, 154, 190, 330, 410];
+
+        var dataElement = document.getElementById('data');
+        // x轴标签
+        var labelsDataAttribute  = dataElement.getAttribute('data-labels');
+        // 将获取到的字符串转换为数组（假设日期以逗号分隔）
+        var labels = labelsDataAttribute.split(',');
+        // 将日期格式转换为指定格式
+        var weekXAxisData = labels.map(function(date) {
+          return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit' });
+        });
+        // 打印转换后的数组标签
+        // console.log(weekXAxisData);
+
+
+
+
+        //数据
+        var weekData = dataElement.getAttribute('data-task-total-time');
+        var monthData = dataElement.getAttribute('data-break-total-time');
+
+        // weekData数据处理
+        var weekDatass = weekData.replace(/[\(\)']+/g, '');
+        console.log(weekDatass)
+
+        // monthData数据处理
+        var monthDatass = monthData.replace(/[\(\)']+/g, '');
+
+        console.log(monthDatass)
+
+        // 数据转换
+        // var weekDataArray = JSON.parse("[" + weekData + "]");
+        // var yearDataArray = JSON.parse("[" + yearData + "]");
+        // console.log(dataArray)
+        // console.log(yearDataArray)
+
+
+        // x轴
+        var monthXAxisData = ['第一周', '第二周', '第三周', '第四周'];
+        // var yearXAxisData = ['2024-03-07', '2024-03-08', '2024-03-09', '2024-03-10', '2024-03-11', '2024-03-12', '2024-03-13'];
 
         option = {
             tooltip: {
@@ -52,31 +91,31 @@
             ],
             series: [
                 {
-                    name: 'Email',
+                    name: '周',
                     type: 'bar',
                     stack: 'Ad',
                     emphasis: {
                         focus: 'series'
                     },
-                    data: weekData
+                    data: weekDatass
                 },
                 {
-                    name: 'Union Ads',
+                    name: '月',
                     type: 'bar',
                     stack: 'Ad',
                     emphasis: {
                         focus: 'series'
                     },
-                    data: [120, 132, 101, 134, 90, 230, 210]// 堆叠数据
+                    data: monthDatass  // 堆叠数据
                 },
                 {
-                    name: 'Video Ads',
+                    name: '年',
                     type: 'bar',
                     stack: 'Ad',
                     emphasis: {
                         focus: 'series'
                     },
-                    data: [120, 132, 101, 134, 90, 230, 210] // 堆叠数据
+                    data: monthDatass // 堆叠数据
                 }
             ]
         };
@@ -96,12 +135,12 @@
 
         document.getElementById('monthBtn').addEventListener('click', function () {
             option.xAxis[0].data = monthXAxisData;
-            option.series[0].data = monthData;
+            option.series[0].data = monthDatass;
             myChart.setOption(option);
         });
 
         document.getElementById('yearBtn').addEventListener('click', function () {
-            option.xAxis[0].data = yearXAxisData;
-            option.series[0].data = yearData;
+            option.xAxis[0].data = monthXAxisData;
+            option.series[0].data = monthDatass;
             myChart.setOption(option);
         });
