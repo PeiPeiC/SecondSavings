a065ee9665795d081d7e90f7f100f0492ad9a53f
Lixingwan Cao <2750536c@student.gla.ac.uk>
2024-03-14 00:11:28 +0000

    fix google select

--- TimeTracker/views.py
+++ TimeTracker/views.py
@@ -863,7 +863,7 @@ def update_sync_settings(request):
 
 def is_google_user(user):
     try:
-        _ = SocialAccount.objects.get(provider='google', user=user)
+        _ = SocialAccount.objects.get(provider__iexact='google', user=user)
         return True
     except SocialAccount.DoesNotExist:
         return False
